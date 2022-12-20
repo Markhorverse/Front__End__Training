f03dbaba086f5ac3a8a9e204e77737fb81f02eb9
Mohummad Abdullah <114979134+abdullahprogrammerweb@users.noreply.github.com>
2022-12-20 14:09:10 +0500

    JavaScript tutorial files
    This is 6 files upload today

---
+++ 3 JavaScript/datatypes.js
@@ -0,0 +1,10 @@
+var age = 25; // Number
+var name = "John"; // String
+var developer = true;// Boolean
+var location = null; // Null
+var task; // undefined
+
+// this is widly used datatypes in the js
+
+
+
